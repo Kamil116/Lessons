e995f6775ecca50c97669c1748ffb863c70a2624
Kamil <k.mirgasimov@innopolis.university>
2025-03-16 01:30:15 +0300

    16.03: исправление решения

--- 16.03/24/7966_kpolyakov/[Решение куратора]7966_kpolyakov.py
+++ 16.03/24/7966_kpolyakov/[Решение куратора]7966_kpolyakov.py
@@ -5,6 +5,6 @@ s = f.readline()
 
 # неотр. число из 3сс *+ неотр. число из 3сс
 num = '[1-2][0-2]*|0'
-pattern = f'(?:{num})(?:[*+](?:{num})*)'
+pattern = f'(?:{num})(?:(?:[*+]{num})*)'
 res = findall(pattern, s)
 print(len(max(res, key=len)))
