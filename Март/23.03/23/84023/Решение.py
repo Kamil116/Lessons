162f5ec0e72599fc84cefb22527385acf012fb44
Kamil <k.mirgasimov@innopolis.university>
2025-03-22 06:17:43 +0300

    23.03: новый способ для 23

--- Март/23.03/23/84023/Решение.py
+++ Март/23.03/23/84023/Решение.py
@@ -1,15 +1,27 @@
-def func(a, b, c1, c2, c3):
-    if a > b:
+# # Способ 1
+# def func(a, b, c1, c2, c3):
+#     if a > b:
+#         return 0
+#     if a == b:
+#         return 1
+#     if c1 == c2 == c3:
+#         if c3 == 1:
+#             return func(a * 2, b, 1, 1, 2)
+#         else:
+#             return func(a + 1, b, 2, 2, 1)
+#     else:
+#         return func(a + 1, b, c2, c3, 1) + func(a * 2, b, c2, c3, 2)
+#
+#
+# print(func(5, 299, -1, 0, 0))
+
+# Способ 2
+def func(a, b, moves):
+    if a > b or '1111' in moves or '2222' in moves:
         return 0
     if a == b:
         return 1
-    if c1 == c2 == c3:
-        if c3 == 1:
-            return func(a * 2, b, 1, 1, 2)
-        else:
-            return func(a + 1, b, 2, 2, 1)
-    else:
-        return func(a + 1, b, c2, c3, 1) + func(a * 2, b, c2, c3, 2)
+    return func(a + 1, b, moves + '1') + func(a * 2, b, moves + '2')
 
 
-print(func(5, 299, -1, 0, 0))
+print(func(5, 299, ''))
