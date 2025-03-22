8a044fb7d6f39e41d9c0cf0129bc14c5f65ea377
Kamil <k.mirgasimov@innopolis.university>
2025-03-22 06:39:10 +0300

    23.03: новый способ для 24 (два указателя)

--- Март/23.03/24/Не регулярки/20909_kompege/[Решение куратора]20909_kompege.py
+++ Март/23.03/24/Не регулярки/20909_kompege/[Решение куратора]20909_kompege.py
@@ -1,3 +1,4 @@
+# Способ 1 через split
 f = open('24_20909.txt')
 s = f.readline()
 s = s.split('AB')
@@ -5,3 +6,19 @@ maxim = 0
 for i in range(len(s) - 100):
     maxim = max(maxim, sum(map(len, s[i:i + 100 + 1])) + 2 + 100 * 2)
 print(maxim)
+
+# # Способ 2 через два указателя
+# f = open('24_20909.txt')
+# s = f.readline()
+# # s = 'xxABxxxABxABxxx'
+# # xxABxxxABxABxxx
+# left = right = count_ab = maxim = 0
+# for right in range(len(s) - 1):
+#     if s[right:right + 2] == 'AB':
+#         count_ab += 1
+#     while count_ab == 101:
+#         maxim = max(maxim, right - left + 1)
+#         if s[left:left + 2] == 'AB':
+#             count_ab -= 1
+#         left += 1
+# print(maxim)
