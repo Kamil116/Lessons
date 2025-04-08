432802c922779002b50e109c17a37839a7f97ec6
Kamil <k.mirgasimov@innopolis.university>
2025-04-09 02:37:17 +0300

    09.04: решения

---
+++ Апрель/09.04/19-21/76982.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+
+@lru_cache(None)
+def game(a, b, c):
+    if a + b + c >= 150:
+        return 0
+    moves = [game(a + 16, b, c), game(a, b + 16, c), game(a, b, c + 16),
+             game(a + 32, b, c), game(a, b + 32, c), game(a, b, c + 32),
+             game(a * 2, b, c), game(a, b * 2, c), game(a, b, c * 2)]
+    win = [pos for pos in moves if pos <= 0]
+    if win:
+        return -max(win) + 1
+    else:
+        return -max(moves)
+
+
+for s in range(1, 67):
+    if game(6, 2 * s, 3 * s) == -2:
+        print(s)
---
+++ Апрель/09.04/26/20970_kompege/20970_kompege.py
@@ -0,0 +1,42 @@
+f = open('26_20970.txt')
+n, k = map(int, f.readline().split())
+m = [[] for _ in range(k)]
+clients = [list(map(int, line.split())) for line in f.readlines()]
+clients.sort()
+util = 0
+for i in range(len(clients)):
+    start, duration, need_num = clients[i]
+    if need_num == 0:
+        min_queue_num = 10000
+        min_queue_ind = None
+        for j in range(len(m)):
+            queue_num = 0
+            for k in range(len(m[j])):
+                if start < m[j][k]:
+                    queue_num += 1
+            if queue_num < min_queue_num:
+                min_queue_num = queue_num
+                min_queue_ind = j
+        if min_queue_num < 5:
+            if m[min_queue_ind] == []:
+                cur_end = start + duration
+            else:
+                cur_end = max(max(m[min_queue_ind]), start) + duration
+            m[min_queue_ind].append(cur_end)
+        else:
+            util += 1
+    else:
+        queue_num = 0
+        for k in range(len(m[need_num - 1])):
+            if start < m[need_num - 1][k]:
+                queue_num += 1
+        if queue_num < 5:
+            if m[need_num - 1] == []:
+                cur_end = start + duration
+            else:
+                cur_end = max(max(m[need_num - 1]), start) + duration
+            m[need_num - 1].append(cur_end)
+        else:
+            util += 1
+print(len(max(m, key=len)))
+print(util)
